289732b08cfe87f8958817f1fe86d453bf61e76e
ahmedelarabykhaled <ahmedelarabykhaled2020@yahoo.com>
2021-04-22 01:21:25 +0200

    trained models

--- main/views.py
+++ main/views.py
@@ -2,13 +2,28 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-from .includes.getWeather import ModelsPredict
+
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout, Bidirectional
+from keras.models import load_model
+import numpy as np
+from .getWeather import ModelsPredict
 
-# Create your views here.
+path = r"D:\Ahmed Khaled\graduation project\django project\WeatherForecasting\main"
 
-def prepare_dataset() :
-    dataset = pd.read_csv('includes/models/weatherHistory.csv')
+
+def index(response) :
+    # predict = ModelsPredict(,[])
+    return HttpResponse(
+        '<div style="display:flex;justify-content:center;align-items:center;height:500px;flex-direction:column;"><h1 style="font-size:6em;font-family: \'Roboto\', sans-serif;">Home Page</h1><p style="font-family: \'Open Sans\', sans-serif;">Ahmed Khaled</p></div>')
+
+def home(response) :
+    return HttpResponse("welcome here form the home page")
+
+
+def test(response) :
+    dataset = pd.read_csv(path+'\weatherHistory.csv')
     final_summary = pd.DataFrame(dataset)
     unique_values = final_summary.Summary.unique()
     final_summary.Summary = pd.Categorical(final_summary.Summary)
@@ -21,14 +36,8 @@ def prepare_dataset() :
     final_precip_type['Precip Type'] = final_precip_type['Precip Type'].cat.codes
     tempreature_training_set = final_precip_type.iloc[:,1:11]
 
-
-last_dataset = prepare_dataset()
-
-
-def index(response) :
-    # predict = ModelsPredict(,[])
-    return HttpResponse(
-        '<div style="display:flex;justify-content:center;align-items:center;height:500px;flex-direction:column;"><h1 style="font-size:6em;font-family: \'Roboto\', sans-serif;">Home Page</h1><p style="font-family: \'Open Sans\', sans-serif;">Ahmed Khaled</p></div>')
-
-def home(response) :
-    return HttpResponse("welcome here form the home page")
+    tempreature_training_set = np.array(tempreature_training_set)
+    days = tempreature_training_set[:, 0:11]
+    ModelWeather = ModelsPredict(days , [])
+    tempreature = ModelWeather.PredictTempreture()
+    return HttpResponse(tempreature)
