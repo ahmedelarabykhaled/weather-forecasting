b516a31fc284b897bf5494d47a3ca8aa544a966f
ahmedelarabykhaled <ahmedelarabykhaled2020@yahoo.com>
2021-04-22 00:29:20 +0200

    add files

--- main/views.py
+++ main/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .includes.getWeather import ModelsPredict
+
 
 # Create your views here.
 
+
 def index(response) :
+    predict = ModelsPredict()
     return HttpResponse(
         '<div style="display:flex;justify-content:center;align-items:center;height:500px;flex-direction:column;"><h1 style="font-size:6em;font-family: \'Roboto\', sans-serif;">Home Page</h1><p style="font-family: \'Open Sans\', sans-serif;">Ahmed Khaled</p></div>')
 
